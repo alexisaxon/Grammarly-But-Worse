06e5fd2dce4a913232f6f9dd413fcb049d09034a
Alexis <aaxon@andrew.cmu.edu>
2020-11-28 18:33:33 -0500

    Progress on text editor

--- frontend.py
+++ frontend.py
@@ -1,9 +1,7 @@
 import module_manager
 module_manager.review()
 from cmu_112_graphics import *
-import tkinter as tk
-import tkinter.scrolledtext as st 
-from tkinter import ttk 
+import grammar
 
 def appStarted(app):
     app.buttons = []
@@ -11,14 +9,15 @@ def appStarted(app):
     app.buttonMargin = 10
     app.selectedWord = None #will be index
     app.words = ["Insert", "your", "text", "here."] #list of words in document
+    app.wordsRepr = ["Insert", "your", "text", "here."] #includes escape sequences
     app.buttonHeight = 30
     app.textboxSelected = False
     app.fullText = "Insert your text here."
     app.maxChars = int(round(app.width/11))
-
-
-def mousePressed(app, event):
-    pass
+    app.textboxBorders = (13, 25 + app.height//15, app.width//2 - 13, app.height - 20)
+    # (x0, y0, x1, y1) of textbox borders
+    app.cursorLocation = 0 #string index
+    app.cursorChar = "|"
 
 #returns True if user clicked button with 'text', false otherwise    
 def buttonClicked(app, text, x, y):
@@ -33,9 +32,52 @@ def buttonClicked(app, text, x, y):
     except:
         print("text not in button list")
 
+def typeACharacter(app, c):
+    app.fullText = app.fullText[0:app.cursorLocation] + c + \
+        app.fullText[app.cursorLocation:]
+    app.cursorLocation += 1
+    createWordList(app, app.fullText)
+    app.wordsRepr = app.fullText.split(" ")
+
+def createWordList(app, text):
+    app.words = app.fullText.split(" ")
+    for i in range(len(app.words)):
+        if "\n" in app.words[i]:
+            temp = app.words[i]
+            index = temp.find("\n")
+            app.words[i] = temp[0:index]
+            app.words.insert(i+1,temp[index+1:]) 
+
+#allows user to type in textbox
+#Professor Kosbie gave me high-level advice on how to implement the textbox in 112 Graphics
+#To do: figure out how to implement shift+letter for uppercase
+def keyPressed(app, event):
+    if app.textboxSelected:
+        if event.key in "abcdefghijklmnopqrstuvwxyz./,!@#$%^&*()_-+=\][}{';:><?\"\'\\":
+            typeACharacter(app, event.key)
+        elif event.key == "Space":
+            typeACharacter(app, " ")
+        elif event.key == "Enter":
+            app.fullText = app.fullText[0:app.cursorLocation] + "\n" + \
+                app.fullText[app.cursorLocation:]
+            createWordList(app, app.fullText)
+            app.wordsRepr = app.fullText.split(" ")
+            print(app.words)
+        elif event.key == "Backspace" and app.cursorLocation != 0:
+            app.fullText = app.fullText[0:app.cursorLocation - 1] + \
+                app.fullText[app.cursorLocation:]
+            app.cursorLocation -= 1
+            app.words = app.fullText.split(" ")
+        elif event.key == "Left" and app.cursorLocation != 0:
+            app.cursorLocation -= 1
+        elif event.key == "Right" and app.cursorLocation < len(app.fullText):
+            app.cursorLocation += 1
+
 def mousePressed(app, event):
+    x0, y0, x1, y1 = app.textboxBorders
     #don't do button checks if on other side of canvas
     if event.x > app.width//2 + app.buttonMargin: 
+        app.textboxSelected = False
         for text in app.buttons:
             if buttonClicked(app, text, event.x, event.y):
                 try:
@@ -43,6 +85,10 @@ def mousePressed(app, event):
                         app.words[app.selectedWord:].replace(app.words[app.selectedWord], text, 1)
                 except:
                     print("Error: selectedWord probably not defined")
+    elif x1 >= event.x >= x0 and y1 >= event.y >= y0:
+        app.textboxSelected = True
+    else:
+        app.textboxSelected = False
 
 #should call when new word selected
 def calculateButtonLocations(app):
@@ -70,14 +116,14 @@ def drawButtons(app, canvas):
 #may want to move the calculations elsewhere
 def drawTextbox(app, canvas):
     startHeight = 25 + app.height//15
-    canvas.create_rectangle(13, startHeight, app.width//2 - 13, app.height - 20, fill='lightgrey', \
+    canvas.create_rectangle(app.textboxBorders, fill='lightgrey', \
         outline='lightgrey')
     lineTotal = 0
     startX = 15
     startY = startHeight
     currentLine = 0
     textLines = [""]
-    for word in app.words:
+    for word in app.wordsRepr:
         if lineTotal + len(word) > app.maxChars:
             textLines.append(word)
             lineTotal = len(word)
@@ -90,8 +136,8 @@ def drawTextbox(app, canvas):
         toPrint += line + "\n"
     canvas.create_text(startX, startY, text=toPrint, anchor="nw")
 
-
-def redrawAll(app, canvas):
+#also includes top label
+def drawBorders(app, canvas):
     canvas.create_rectangle(0, 0, app.width, app.height, outline="lightyellow", fill="lightyellow")
     canvas.create_rectangle(5, 5, app.width - 5, app.height - 5, outline='red')
     canvas.create_rectangle(6, 6, app.width - 6, app.height - 6, outline='orange')
@@ -101,6 +147,9 @@ def redrawAll(app, canvas):
     fontSize = int(round(app.height/22.86))
     canvas.create_text(app.width//2, app.height//30 + 13, text="Grammarly But Worse", \
         font=f"Times {fontSize} bold")
+
+def redrawAll(app, canvas):
+    drawBorders(app, canvas)
     drawButtons(app, canvas)
     drawTextbox(app, canvas)
 
